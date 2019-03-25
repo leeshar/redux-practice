a60886749e3a9fe02b27f32894145e44ef167463
snrntpa2z@naver.com <snrntpa2z@naver.com>
2019-03-25 18:08:12 +0900

    3/25 : redux-todo-list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import Footer from "./components/Footer";
+import AddTodo from "./containers/AddTodo";
+import VisibleTodoList from "./containers/VisibleTodoList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h3>REDUX</h3>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
       </div>
     );
   }
